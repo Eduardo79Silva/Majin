99fded59936d41b0215e87e2ad556ba4818e4dc5
Eduardo Silva <eduardo4silva@gmail.com>
2025-09-24 11:53:53 +0200

    feat: allow input of color to the vertex shader
    The vertex shader in turn passes it to the fragment shader which displays the colors using lerp with barycentric coordinates

--- engine/first_app.cpp
+++ engine/first_app.cpp
@@ -124,8 +124,9 @@ void FirstApp::drawFrame() {
 }
 
 void FirstApp::loadModels() {
-  std::vector<MajinModel::Vertex> vertices{
-      {{0.0f, -0.5f}}, {{0.5f, 0.5f}}, {{-0.5f, 0.5f}}};
+  std::vector<MajinModel::Vertex> vertices{{{0.0f, -0.5f}, {1.0f, 0.0f, 0.0f}},
+                                           {{0.5f, 0.5f}, {0.0f, 1.0f, 0.0f}},
+                                           {{-0.5f, 0.5f}, {0.0f, 0.0f, 1.0f}}};
 
   _majinModel = std::make_unique<MajinModel>(_majinDevice, vertices);
 }
