1e788a7cf63a7b7ab2e7dc595ecdcd57faebdd7d
Eduardo Silva <eduardo.silva@mieterengel.de>
2025-12-10 17:25:17 +0100

    feat: implementation of gravity system (not working yet)

--- engine/first_app.cpp
+++ engine/first_app.cpp
@@ -1,6 +1,10 @@
 #include "first_app.hpp"
 
+#include "gravity_system.hpp"
+#include "majin_model.hpp"
 #include "simple_render_system.hpp"
+#include <memory>
+#include <vector>
 
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
@@ -15,7 +19,80 @@ FirstApp::FirstApp() { loadGameObjects(); }
 
 FirstApp::~FirstApp() {}
 
+std::unique_ptr<MajinModel> createSquareModel(MajinDevice &device,
+                                              glm::vec2 offset) {
+  std::vector<MajinModel::Vertex> vertices = {
+      {{-0.5f, -0.5f}}, {{0.5f, 0.5f}},  {{-0.5f, 0.5f}},
+      {{-0.5f, -0.5f}}, {{0.5f, -0.5f}}, {{0.5f, 0.5f}}, //
+  };
+  for (auto &v : vertices) {
+    v.position += offset;
+  }
+  return std::make_unique<MajinModel>(device, vertices);
+}
+
+std::unique_ptr<MajinModel> createCircleModel(MajinDevice &device,
+                                              unsigned int numSides) {
+  std::vector<MajinModel::Vertex> uniqueVertices{};
+
+  for (int i = 0; i < numSides; i++) {
+    float angle = i * glm::two_pi<float>() / numSides;
+    uniqueVertices.push_back({{glm::cos(angle), glm::sin(angle)}});
+  }
+
+  uniqueVertices.push_back({});
+  std::vector<MajinModel::Vertex> vertices{};
+
+  for (int i = 0; i < numSides; i++) {
+    vertices.push_back(uniqueVertices[i]);
+    vertices.push_back(uniqueVertices[(i + 1) % numSides]);
+    vertices.push_back(uniqueVertices[numSides]);
+  }
+
+  return std::make_unique<MajinModel>(device, vertices);
+}
+
 void FirstApp::run() {
+  std::shared_ptr<MajinModel> squareModel =
+      createSquareModel(_majinDevice, {.5f, .0f});
+  std::shared_ptr<MajinModel> circleModel = createCircleModel(_majinDevice, 64);
+
+  std::vector<MajinGameObject> physicsObjects{};
+
+  auto red = MajinGameObject::createGameObject();
+  red.transform2D.scale = glm::vec2{.05f};
+  red.transform2D.translation = {.5f, .5f};
+  red.color = {1.f, 0.f, 0.f};
+  red.rigidBody2D.velocity = {-.5f, .0f};
+  red.model = circleModel;
+  physicsObjects.push_back(std::move(red));
+
+  auto blue = MajinGameObject::createGameObject();
+  blue.transform2D.scale = glm::vec2{.05f};
+  blue.transform2D.translation = {-.45f, -.25f};
+  blue.color = {0.f, 0.f, 1.f};
+  blue.rigidBody2D.velocity = {.5f, .0f};
+  blue.model = circleModel;
+  physicsObjects.push_back(std::move(blue));
+
+  std::vector<MajinGameObject> vectorField{};
+  int gridCount = 40;
+
+  for (int i = 0; i < gridCount; i++) {
+    for (int j = 0; j < gridCount; j++) {
+      auto vf = MajinGameObject::createGameObject();
+      vf.transform2D.scale = glm::vec2(0.005f);
+      vf.transform2D.translation = {-1.0f + (i + 0.5f) * 2.0f / gridCount,
+                                    -1.0f + (j + 0.5f) * 2.0f / gridCount};
+      vf.color = glm::vec3(1.0f);
+      vf.model = squareModel;
+      vectorField.push_back(std::move(vf));
+    }
+  }
+
+  GravityPhysicsSystem gravitySystem{0.81f};
+  Vec2FieldSystem vecFieldSystem{};
+
   SimpleRenderSystem simpleRenderSystem{
       _majinDevice, _majinRenderer.getSwapChainRenderPass()};
 
@@ -23,8 +100,12 @@ void FirstApp::run() {
     glfwPollEvents();
 
     if (auto commandBuffer = _majinRenderer.beginFrame()) {
+      gravitySystem.update(physicsObjects, 1.f / 60, 5);
+      vecFieldSystem.update(gravitySystem, physicsObjects, vectorField);
+
       _majinRenderer.beginSwapChainRenderPass(commandBuffer);
-      simpleRenderSystem.renderGameObjects(commandBuffer, _gameObjects);
+      simpleRenderSystem.renderGameObjects(commandBuffer, physicsObjects);
+      simpleRenderSystem.renderGameObjects(commandBuffer, vectorField);
       _majinRenderer.endSwapChainRenderPass(commandBuffer);
       _majinRenderer.endFrame();
     }
